6db7c017ecef4215361974c9ea74845a683c82bb
Jorge Sanz <jsanzbriones@gmail.com>
2020-04-05 19:45:03 +0200

    Initial commit

---
+++ src/test/java/es/jorgesanz/alemixer/AlemixerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.jorgesanz.alemixer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlemixerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
