300ba6d8abd83524f61edb939cc32433e90f7e1b
Unknown <jsanzbriones@gmail.com>
2020-04-10 09:55:36 +0200

    initial service call

--- src/main/java/es/jorgesanz/alemixer/AlemixerApplication.java
+++ src/main/java/es/jorgesanz/alemixer/AlemixerApplication.java
@@ -1,13 +1,24 @@
 package es.jorgesanz.alemixer;
 
+import es.jorgesanz.alemixer.service.AleMixerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class AlemixerApplication {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	public static void main(String[] args) {
-		SpringApplication.run(AlemixerApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(AlemixerApplication.class, args);
+		AleMixerService aleMixerService =  context.getBean(AleMixerService.class);
+		aleMixerService.mix();
+
 	}
 
 }
---
+++ src/main/java/es/jorgesanz/alemixer/service/AleMixerService.java
@@ -0,0 +1,10 @@
+package es.jorgesanz.alemixer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AleMixerService {
+    public void mix(){
+        System.out.println("MIX!!");
+    }
+}
