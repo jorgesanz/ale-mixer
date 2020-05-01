e9a39e74074c22987a5b0f541630329531043633
Jorge Sanz Briones <jorgesbr@ext.inditex.com>
2020-05-01 19:34:05 +0200

    write test file

--- src/main/java/es/jorgesanz/alemixer/service/OutputAleFileWriterService.java
+++ src/main/java/es/jorgesanz/alemixer/service/OutputAleFileWriterService.java
@@ -1,11 +1,26 @@
 package es.jorgesanz.alemixer.service;
 
 import es.jorgesanz.alemixer.model.AleFile;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Service
-public class OutputAleFileWriterService {
-    public void write(AleFile replacedAleFile, String s) {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
 
+@Service
+@Slf4j
+public class OutputAleFileWriterService {
+    public void write(AleFile replacedAleFile, String fileLocation) {
+        try{
+            File file = new File(fileLocation);
+            Writer output = new BufferedWriter(new FileWriter(file));
+            output.write("File Content");
+            output.close();
+
+        }catch(Exception e){
+            log.error(String.format("error loading file %s", fileLocation),e.getMessage());
+        }
     }
 }
